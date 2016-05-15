eda60a6819dcf0577fe68278bef9cbc7760e9bb4
Ryan Morr <rm.morr@gmail.com>
2016-05-15 15:08:38 -0400

    Add exports and UMD babel plugins

--- src/dominate.js
+++ src/dominate.js
@@ -23,7 +23,7 @@ wrapMap.th = wrapMap.td;
  * @return {Element|TextNode|DocumentFragment}
  * @api public
  */
-function dominate(html, doc = document) {
+export default function dominate(html, doc = document) {
     // Validate html param
     if (~~('string boolean number').indexOf(typeof html)) {
         throw new TypeError('Invalid input, string/number/boolean expected');
@@ -57,13 +57,4 @@ function dominate(html, doc = document) {
         frag.appendChild(el.firstChild);
     }
     return frag;
-}
-
-// Expose `dominate`
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = dominate;
-} else if(typeof define === 'function' && define.amd) {
-    define(function(){ return dominate; });
-} else {
-    window['dominate'] = dominate;
 }
