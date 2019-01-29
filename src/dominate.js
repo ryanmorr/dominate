a89e6ac3fa1faa7580ea9c170d48448cf9e1eb08
Ryan Morr <rm.morr@gmail.com>
2019-01-28 20:15:38 -0500

    Remove comments

--- src/dominate.js
+++ src/dominate.js
@@ -189,21 +189,13 @@ function parseHTML(doc, html, tag = 'div') {
  * @api private
  */
 function parseElements(doc, tag, html) {
-    // Use the <template> element if it is supported and
-    // the tag is not an SVG element
     if (supportsTemplate && !isSVG(tag)) {
-        // Create a template element to parse the HTML string
         const template = doc.createElement('template');
         template.innerHTML = html;
-        // Clone and return the document fragment within
-        // the template
         return doc.importNode(template.content, true);
     }
-    // Wrap the element in the appropriate container
     const wrap = wrapMap[tag] || wrapMap._default;
-    // Parse HTML string
     let el = parseHTML(doc, wrap[1] + html + wrap[2]);
-    // Descend through wrappers to get the right element
     let depth = wrap[0];
     while (depth--) {
         el = el.lastChild;
@@ -221,34 +213,25 @@ function parseElements(doc, tag, html) {
  * @api private
  */
 function parse(doc, tag, html) {
-    // Support <html> elements
     if (tag === 'html') {
         if (supportsDOMParserHTML) {
             return parseDocument(html, 'text/html');
         }
-        // Attributes of the <html> element do not get
-        // parsed using `innerHTML` here, so we parse it
-        // as XML and then copy the attributes
         const el = parseHTML(doc, html, 'html');
         const xml = parseDocument(html, 'text/xml');
         return copyAttributes(el, xml);
     }
-    // Support <body> and <head> elements
     if (tag === 'head' || tag === 'body') {
         const el = parseHTML(doc, html, 'html');
         return el.removeChild(tag === 'head' ? el.firstChild : el.lastChild);
     }
-    // Support every other element
     const el = parseElements(doc, tag, html);
-    // Support executable <script> elements
     if (tag === 'script') {
         return copyScript(doc, el.firstChild);
     }
-    // Single element
     if (el.childNodes.length === 1) {
         return el.removeChild(el.firstChild);
     }
-    // Use a document fragment for multiple elements
     const frag = doc.createDocumentFragment();
     while (el.firstChild) {
         frag.appendChild(el.firstChild);
@@ -268,34 +251,22 @@ function parse(doc, tag, html) {
  * @api public
  */
 export default function dominate(html, {context = document, type = 'html', scripts = true} = {}) {
-    // Check if it is a tagged template invocation
     if (isTaggedTemplate(arguments)) {
         const [strings, ...values] = arguments;
         return dominate(strings.raw.reduce((acc, str, i) => acc + (values[i - 1]).join('') + str));
     }
-    // Return an XML element if the type param is 'xml'
     if (type.toLowerCase() === 'xml') {
         return parseDocument(html, 'text/xml');
     }
-    // Parse the HTML string for a tag name
     const match = tagNameRe.exec(html);
-    // If no tag name exists, treat it as plain text
     if (!match) {
         return context.createTextNode(html);
     }
-    // Get the tag name
     const tag = match[1];
-    // Parse the HTML string into a DOM node
     const el = parse(context, tag, html.trim());
-    // If it's a script element, return it as it
-    // should always execute regardless of the
-    // `scripts` param
     if (tag === 'script') {
         return el;
     }
-    // If `scripts` param is true, replace all script
-    // elements with a new script element to enable
-    // execution, otherwise remove the script elements
     const elements = el.querySelectorAll('script');
     for (let i = 0, len = elements.length, script, parent; i < len; i++) {
         script = elements[i];
